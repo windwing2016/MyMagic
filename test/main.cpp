1d5ce4e7ef77194b4a9160b97450e41e3d63258b
windwing <78356388@qq.com>
2016-01-28 18:54:08 +0800

    default

---
+++ test/main.cpp
@@ -0,0 +1,24 @@
+#include <QCoreApplication>
+#include <../McfCore/mcf.h>
+#include <../McfCore/mcfcore.h>
+#include <../McfCore/mcf/ViewModels/NotifiableProperties.h>
+#include <QDebug>
+
+using namespace std;
+using namespace Mcf;
+
+//Test temp;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+  //  qRegisterMetaType<Test>("Test");
+      Test temp;
+//      MCF_DOUBLE result=temp.add(3.3,3.3);
+      temp.add(3,4);
+//    qDebug()<<result;
+
+
+
+    return a.exec();
+}
